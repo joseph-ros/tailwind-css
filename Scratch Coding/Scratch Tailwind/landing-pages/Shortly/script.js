addd5f7630e6801e0c0fb2db02dddfc61c5d89d8
Joseph Ros <rosjoseph.tran@gmail.com>
2026-08-19 20:26:29 +0700

    init

--- Scratch Coding/Scratch Tailwind/landing-pages/Shortly/script.js
+++ Scratch Coding/Scratch Tailwind/landing-pages/Shortly/script.js
@@ -8,14 +8,12 @@ const errMsg = document.getElementById('err-msg')
 btn.addEventListener('click', navToggle)
 linkForm.addEventListener('submit', formSubmit)
 
-// Toggle Mobile Menu
 function navToggle() {
   btn.classList.toggle('open')
   menu.classList.toggle('flex')
   menu.classList.toggle('hidden')
 }
 
-// Validate a URL
 function validURL(str) {
   var pattern = new RegExp(
     '^(https?:\\/\\/)?' + // protocol
